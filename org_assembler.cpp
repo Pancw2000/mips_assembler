31e77e9d0b3177eef17d2dbae2f5f6073265a12c
pancw <panchuwen2000@gmail.com>
2020-09-16 21:49:26 +0800

    able to turn lw add addi into machine codes.

--- org_assembler.cpp
+++ org_assembler.cpp
@@ -1,42 +1,19 @@
 #include <string>
-#include <cstdio>
-#include <cstring>
 #include <map>
 #include <vector>
 #include <iostream>
+#include <fstream>
 #include <bitset>
 using namespace std;
 
-// in.txt out.txt
-//for testing no need for cmd compile + arg, use IDE instead
-int main(){
-    FILE *inFile;
-    FILE *outFile;
-    long cur_PC = 0; //PC 32bit int/long/bitvector?
-    inFile = fopen ("in.txt","r");
-    outFile = fopen("out.txt","w");
-    char inst[60];
-    if(!inFile){
-        cout<< "file input error";
-        //error
-    }
-    else{
-        while(fgets(inst,60,inFile)!=NULL){
-            assemble(inst, cur_PC, outFile);
-        }
-       
-    }
-}
 
 //pass1
-
 //pass2
 
 
 //不管注释
 //已经读入一条指令
-
-
+map<string,int> label_addr_table; //在第一遍遍历指令时，创建addr_table
 map<string,string> R_func_map = {
     {"add","100000"},
     {"addu","100001"},
@@ -59,6 +36,8 @@ map<string,string> R_func_map = {
 };
 
 map<string,string> opcode_map = {
+    {"R","000000"},
+    {"lw","100011"},
     {"addi","001000"},
     {"addiu","001001"},
     {"andi","001100"},
@@ -81,42 +60,111 @@ map<string,string> opcode_map = {
     {"jr","001000"}
 };
 
-map<string,int> reg_map;
+map<string,string> reg_map = {
+    {"$zero","00000"},
+    {"$at","00001"},
+    {"$v0","00010"},
+    {"$v1","00011"},
+    {"$a0","00100"},
+    {"$a1","00101"},
+    {"$a2","00110"},
+    {"$a3","00111"}
+    //todo
+};
+
+class addr_32bit{
+    public:
+        addr_32bit(){
+            v1 =  vector<bool>(32,false);
+        }
+        string get(int begin,int end){
+            string tmp_string;
+            for(int i = begin;i>end;--i){
+                tmp_string.append(v1[begin]?"1" : "0");
+            }
+        }
+
+    private:
+        vector<bool> v1;
+};
+
+enum opcode_string{
+    Elw,
+    Eadd,
+    Eaddi
+//todo
+};
+
+opcode_string hash_opcode(string const& opcode){
+    if(opcode == "lw") return Elw;
+    if(opcode == "add") return Eadd;
+    if(opcode == "addi") return Eaddi;
+//todo
+}
 
 //读入指令，写入out.txt
-void assemble(string instruction, addr_32bit cur_PC,FILE* outFile){
+void assemble(const string& instruction, addr_32bit& cur_PC,ofstream& outfile){
    
-   
-    //parsing the instruction
+    //parse the instruction
     string inst_delimiters = " ,\n\t\r";
-    vector<string> tokens = {NULL};
+    vector<string> tokens; //4 tokens utmost
     
     //multi delims
     int start = 0;
-    int end = instruction.find(inst_delimiters);
+    int end = instruction.find_first_of(inst_delimiters,start);
+    string new_token;
     while(end != string::npos){
-            string new_token = instruction.substr(start,end-start);
+            new_token = instruction.substr(start,end-start);
             tokens.push_back(new_token);
+            start = end + 1;
+            end = instruction.find_first_of(inst_delimiters,start);
     }
+    new_token = instruction.substr(start);
+    tokens.push_back(new_token);
     
-
-
     //up to 4 opcodes  1. opcode+rs+rt+rd 2. opcode+rs+rt+imm(addi) ... 3.opcode+rs+(rt+imm) (lw)
     //5($rs)这种类型还需要进一步parse 
     //需要使用label的跳转指令: beq j jal
-    
 
-
-    string tmp_string;//tmp_string stores binary form, later stored in outFile
-    tmp_string.clear();
-    switch (op1)
+    string out_bitstring;//out_bitstring stores binary form, later stored in outFile            
+    out_bitstring.clear();
+    string rs,rt,rd,imm16,shamt,func;
+    switch (hash_opcode(tokens[0]))
     { 
     //load options    
-        case "lw":
-            string offset = strtok(op3.")(");
-            string rs = strtok(NULL,")(");
-            tmp_string = "100011" + reg_map(rs) + reg_map(op2) + bitset<16>(stoi(offset)).to_string();
+    /*
+        lw $s0,16($t1) tokens[2] = 16($t1)
+        opcode rs,rt,imm16
+    */
+        case Elw:
+        {
+            rt = tokens[1];
+            int paran_pos_0 = tokens[2].find('(',0);
+            int paran_pos_1 = tokens[2].find(')',paran_pos_0+1);
+            imm16 = tokens[2].substr(0,paran_pos_0);
+            rs = tokens[2].substr(paran_pos_0+1,paran_pos_1-paran_pos_0-1);
+            out_bitstring = opcode_map[tokens[0]] + reg_map[rs] + reg_map[rt] + (bitset<16>(stoi(imm16))).to_string();
             break;
+        }
+
+        case Eadd:
+        {
+            rt = tokens[1];
+            rs = tokens[2];
+            rd = tokens[3];
+            out_bitstring = opcode_map["R"] + reg_map[rs] + reg_map[rd] + reg_map[rt] + "00000"+R_func_map[tokens[0]];
+            break;
+        }
+
+        case Eaddi:
+        {
+            rd = tokens[1];
+            rs = tokens[2];
+            imm16 = tokens[3];
+            out_bitstring = opcode_map[tokens[0]] + reg_map[rs] + reg_map[rd] + bitset<16>(stoi(imm16)).to_string();
+            break;
+        }
+        /*
         case "lb":
             string offset = strtok(op3.")(");
             string rs = strtok(NULL,")(");
@@ -276,36 +324,49 @@ void assemble(string instruction, addr_32bit cur_PC,FILE* outFile){
     default:
         break;
     }
-        fputs(tmp_string,outFile);
+    */
+    }
+    cout<<out_bitstring<<endl;
+    outfile << out_bitstring <<endl;
 }
 
 /*需要增加: PC+4 add overload op'+'
     PC: 000000000001010000.... 
   位数: 31 30 29..
 */
-class addr_32bit{
-    public:
-        string get(int begin,int end){
-            string tmp_string;
-            for(int i = begin;i>end;--i){
-                tmp_string.append(v1[begin]?"1" : "0");
-            }
-        }
-
-    private:
-        vector<bool> v1(32);
-}
 
 
-
-map<string,int> label_addr_table; //在第一遍遍历指令时，创建addr_table
 
 
 void ALU_Rtype_Write(string rs,string rt, string rd, string func, string res){
-    res = "000000" + reg_map[rs] + reg_map[rt] + reg_map[rd] + func};
+    res = "000000" + reg_map[rs] + reg_map[rt] + reg_map[rd] + func;
 }
 
 //注意 imm的多种表示方法: 0x23(hex), 12 , 目前将imm视为纯数字10进制
-void ALU_Itype_Write(string opcode, string rt, string rs, string imm, string res){
-    res = opcode_map(opcode) + reg_map(rs) + reg_map(rt) + bitset<16>(stoi(imm)).to_string();
+// void ALU_Itype_Write(string opcode, string rt, string rs, string imm, string res){
+//     res = opcode_map(opcode) + reg_map(rs) + reg_map(rt) + bitset<16>(stoi(imm)).to_string();
+// }
+
+
+// in.txt out.txt
+//for testing no need for cmd compile + arg, use IDE instead
+int main(){
+    ifstream infile("in.txt");
+    ofstream outfile("out.txt");
+    addr_32bit cur_PC;
+    string inst;
+
+    int line_number = 0;
+    if(!infile){
+        cout<< "file input error";
+        //error
+    }
+    else{
+        while(getline(infile,inst)){
+            cout<< line_number++;
+            assemble(inst, cur_PC, outfile);
+        }
+    }
+    infile.close();
+    outfile.close();
 }
